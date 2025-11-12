29a95288cbeba88fd9fed18eba80c65d756fd82e
NicoFerrieri <nferrieri1@studenti.apuliadigitalmaker.it>
2025-11-12 22:13:00 +0100

    Add School scene with clock assets and password UI
    Introduced a new School scene including clock sprites and associated meta files. Added SchoolScript and UI elements for password input and validation.

---
+++ Assets/Scripts/SchoolScript.cs
@@ -0,0 +1,44 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SchoolScript : MonoBehaviour
+{
+    public GameObject drawerPanel;
+    public TMP_InputField passwordInputField;
+    public string correctPassword = "Giulio";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OpenDrawer()
+    {
+        drawerPanel.SetActive(true);
+        Debug.Log("Drawer method called in SchoolScript.");
+    }   
+    
+    public void CheckPassword()
+    {
+        passwordInputField.text = passwordInputField.text.Trim();
+        if(passwordInputField.text.Trim()==correctPassword)
+        {
+            drawerPanel.SetActive(false);
+            Debug.Log("Correct Password!");
+            // Add logic for correct password
+        }
+        else
+        {
+            Debug.Log("Incorrect Password!");
+            // Add logic for incorrect password
+        }
+        Debug.Log("Password checked in SchoolScript.");
+    }
+}
